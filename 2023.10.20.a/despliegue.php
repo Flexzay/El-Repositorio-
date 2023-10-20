34d9a483fe397b298c3104dc385c55ec7d9af1ce
Ricardo <128722871+Flexzay@users.noreply.github.com>
2023-10-20 10:52:15 -0500

    Add files via upload

--- 2023.10.20.a/despliegue.php
+++ 2023.10.20.a/despliegue.php
@@ -3,4 +3,5 @@
 include('funciones.php');
 
 // Se imprime el resultado de llamar a la función 'consulta()'.
-echo consulta('1120561778','ricardorivera10');
+echo consulta('1','andres','2');
+ 
--- 2023.10.20.a/funciones.php
+++ 2023.10.20.a/funciones.php
@@ -3,12 +3,12 @@
 /**
  * Realiza una consulta a la base de datos y devuelve los resultados formateados como una cadena de texto.
  * Autor: Cristian Ricardo Rivera Montañez.
- *
- * @param       string      $u Documento para buscar datos en la tabla usuarios. (Opcional, por defecto es null)
- * @param       string      $c Clave para buscar datos en la tabla usuarios. (Opcional, por defecto es null)
- * @return      string      Devuelve una cadena con los valores de la tabla 'usuarios' separados por saltos de línea.
+ * @param string $u Documento para buscar datos en la tabla usuarios. (Opcional, por defecto es null).
+ * @param sting $c Contraseña para buscar datos en la tabla usuarios. (Opcional, por defecto es null).
+ * @param int $n Número para controlar el tipo de consulta. (Opcional, por defecto es 1).
+ * @return string Devuelve una cadena con los valores de la tabla 'usuarios' separados por saltos de línea.
  */
-function consulta($u = null, $c = null)
+function consulta($u = null, $c = null, $n = 1)
 {
     // Se inicializa una variable para acumular los resultados.
     $salida = "";
@@ -16,16 +16,29 @@ function consulta($u = null, $c = null)
     // Se establece la conexión a la base de datos con el servidor, nombre de usuario, contraseña y nombre de la base de datos.
     $conexion = mysqli_connect('localhost', 'root', 'root', 'practica_');
 
-    // Se verifica si se proporciona un documento y una clave.
-    if ($u != null && $c != null) {
-        // Se define la consulta SQL para seleccionar todos los registros de la tabla 'usuarios'.
-        $sql = "SELECT * FROM usuarios WHERE documento='$u' AND clave='$c'";
+    // Se define la consulta SQL según el valor de $n.
+    if ($n == 1) {
+        // Consulta principal para seleccionar todos los registros de la tabla 'usuarios'.
+        $sql = "SELECT * FROM usuarios";
+        // Si se proporciona un documento, se agrega una condición WHERE a la consulta.
+        if ($u != null) {
+            $sql .= " WHERE documento='$u'";
+        }
+        // Si se proporcionan tanto el documento como la contraseña, se realiza una consulta adicional con ambas condiciones.
+        if ($u != null && $c != null) {
+            $sql = "SELECT * FROM usuarios WHERE documento='$u' AND clave='$c'";
+        }
+    } else if ($n != 1) {
+        // Consulta de recuento para obtener el número total de registros en la tabla 'usuarios'.
+        $sql = "SELECT count(*) FROM usuarios";
+    }
 
-        // Se ejecuta la consulta y se obtiene el conjunto de resultados.
-        $resultado = $conexion->query($sql);
+    // Se ejecuta la consulta y se obtiene el conjunto de resultados.
+    $resultado = $conexion->query($sql);
 
-        // Se verifica si se encontraron resultados.
-        if ($resultado->num_rows > 0) {
+    // Se verifica si se encontraron resultados.
+    if ($resultado) {
+        if ($n == 1) {
             // Se recorren las filas de resultados y se concatenan las columnas a la variable de salida.
             while ($fila = mysqli_fetch_assoc($resultado)) {
                 $salida .= $fila['documento'] . "<br>"; // Concatenación del primer campo
@@ -34,11 +47,12 @@ function consulta($u = null, $c = null)
                 $salida .= $fila['bienvenida'] . "<br>"; // Concatenación del cuarto campo
                 $salida .= $fila['clave'] . "<br>";
             }
-        } else {
-            // No se encontraron resultados, se añade un mensaje indicando que el documento o la clave son incorrectos.
-            $salida .= "Documento o clave incorrectos";
+        } elseif ($n != 1) {
+            // Se obtiene el resultado del recuento.
+            $count = mysqli_fetch_row($resultado)[0];
+            $salida .= "Número total de registros: $count";
         }
-    } 
+    }
 
     // Se cierra la conexión a la base de datos.
     $conexion->close();
